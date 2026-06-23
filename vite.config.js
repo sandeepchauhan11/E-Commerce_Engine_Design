ea5dd95d1522798db53e38c9502dfd5b7d7a962e
sandeepchauhan11 <sandeepchauhan71999@gmail.com>
2026-06-23 23:34:28 +0530

    chore: configure vite base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh' // or @vitejs/plugin-react depending on your install
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // THIS IS THE CRITICAL FIX: Tell Vite your repository subfolder name
+  base: '/E-Commerce_Engine_Design/', 
 })
