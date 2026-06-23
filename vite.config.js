943fea2172a675f097fcc546a2b471afbde803cf
sandeepchauhan11 <sandeepchauhan71999@gmail.com>
2026-06-23 23:43:01 +0530

    fix: update vite react plugin import

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or @vitejs/plugin-react depending on your install
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // THIS IS THE CRITICAL FIX: Tell Vite your repository subfolder name
   base: '/E-Commerce_Engine_Design/', 
 })
